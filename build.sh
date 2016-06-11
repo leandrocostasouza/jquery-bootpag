dc5ef6bdcab74fdfafa30d1f78d478826c482268
leandrocostasouza <leandrocsfrb@gmail.com>
2016-06-11 01:53:55 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Using closure compiler (https://github.com/google/closure-compiler)
 # http://dl.google.com/closure-compiler/compiler-latest.zip
-java -jar ../compiler-latest/compiler.jar --js lib/jquery.bootpag.js --js_output_file lib/jquery.bootpag.min.js
+java -jar ../compiler-latest/compiler.jar --js lib/jquery.w3csspag.js --js_output_file lib/jquery.w3csspag.min.js
